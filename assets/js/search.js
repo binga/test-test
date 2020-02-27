fc3ef2f32875bbb1f916c134e5a660022ed0020c
binga <binga@users.noreply.github.com>
2020-02-27 19:50:44 +0000

    deploy: 26f1188a13440182c86d8de64cb0397c2e72cd45

--- assets/js/search.js
+++ assets/js/search.js
@@ -24,6 +24,11 @@ var documents = [{
     "url": "https://binga.github.io/fastpages/markdown/2020/02/26/Introduction.html",
     "title": "Introduction",
     "body": "2020/02/26 - Testing this section: test, test "
+    }, {
+    "id": 5,
+    "url": "https://binga.github.io/fastpages/markdown/2020/01/31/The-Manual.html",
+    "title": "The Manual By Epictetus",
+    "body": "2020/01/31 -  Stoicism doesn’t mean repressing emotion and shunning pleasure, I learned, but—in essence—focusing on what is in our power and letting go of everything we can’t control.  When practical necessity demands that you desire or avoid something external—at work, for instance—act with steady deliberation, not hasty strain.  People who are ignorant of philosophy blame others for their own misfortunes. Those who are beginning to learn philosophy blame themselves. Those who have mastered philosophy blame no one.  So, if you have not been invited to a party, it is because you haven’t paid the price of the invitation. It costs social engagement, conversation, encouragement, and praise. If you are not willing to pay this price, do not be upset when you don’t receive an invitation.  Whenever misfortune befalls you, ask yourself how you would react if it were someone else in the same situation.  At a feast, taking the largest helping may be good for your appetite, but sharing generously is good for the spirit of the celebration. In this case, honoring your hosts and fellow guests should be valued above sating your hunger.  Care for your body as needed, but put your main energies and efforts into cultivating your mind.  Do not proclaim yourself a philosopher, or go around preaching your principles. Show them by example.  At a feast, do not give a speech about how everyone should eat. Only eat as you should. "
     }];
 
 var idx = lunr(function () {
